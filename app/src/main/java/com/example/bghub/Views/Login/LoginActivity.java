37a90ffbacfb74560c8bb8cc6ebf0a5e7dad6b10
Lucas Galvão <lcollares@indracompany.com>
2020-04-06 22:46:57 -0300

    Adding logout button and fixing login

--- app/src/main/java/com/example/bghub/Views/Login/LoginActivity.java
+++ app/src/main/java/com/example/bghub/Views/Login/LoginActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bghub.R;
 import com.example.bghub.Views.Main.MainActivity;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
+import com.facebook.login.LoginBehavior;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -16,6 +21,9 @@ import com.facebook.login.widget.LoginButton;
 import android.content.Intent;
 import android.os.Bundle;
 
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 import java.util.List;
@@ -44,9 +52,10 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
         }
 
         //Facebook Login
-        List< String > facebookPermissions = Arrays.asList("user_photos", "email","public_profile", "AccessToken");
+        List< String > facebookPermissions = Arrays.asList("user_photos", "email","public_profile");
         mLoginButton = findViewById(R.id.login_button);
         mLoginButton.setPermissions(facebookPermissions);
+        mLoginButton.setLoginBehavior( LoginBehavior.WEB_ONLY );
 
         //FacebookSdk.sdkInitialize(getApplicationContext());
         //LoginManager.getInstance().logOut();
@@ -55,23 +64,12 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-
-                        mPresenter.processUserLogin(Profile.getCurrentProfile(),loginResult.getAccessToken());
-
-                        goToMainActivity();
-
                     }
-
                     @Override
                     public void onCancel() {
-                        // App code
                     }
-
                     @Override
                     public void onError(FacebookException exception) {
-
-                        goToMainActivity();
-                        // App code
                     }
                 });
     }
@@ -86,9 +84,44 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        //finish();
+        finish();
     }
 
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken == null){
 
+            } else{
+                loadUserProfile(currentAccessToken);
 
+                //mPresenter.processUserLogin(Profile.getCurrentProfile(),loginResult.getAccessToken());
+            }
+
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+                    String image_url = "https://graph.facebook.com/" + id + "/picture?type=normal";
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+                goToMainActivity();
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","email,first_name,last_name,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
 }
--- app/src/main/java/com/example/bghub/Views/Main/MainActivity.java
+++ app/src/main/java/com/example/bghub/Views/Main/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.bghub.Views.Main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bghub.R;
+import com.example.bghub.Views.Login.LoginActivity;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -17,6 +21,8 @@ import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
+    Button mLogoutButton;
+
     @Inject
     MainContract.Presenter mPresenter;
 
@@ -25,6 +31,26 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpButtons();
+
+    }
+
+    private void setUpButtons(){
+        mLogoutButton = findViewById(R.id.logout_button);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout(){
+
+        LoginManager.getInstance().logOut();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
 
     }
 }
