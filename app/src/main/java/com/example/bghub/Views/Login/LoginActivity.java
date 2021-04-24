b2c086343379fa9a47e0cfd8d596f836b17b5baa
Lucas Collares Favaron Galvao <lcollares@indracompany.com>
2021-04-24 19:39:30 -0300

    (Migrating_login_to_firebase partial work, added the firebase login UI

--- app/src/main/java/com/example/bghub/Views/Login/LoginActivity.java
+++ app/src/main/java/com/example/bghub/Views/Login/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.bghub.Views.Login;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.example.bghub.R;
 import com.example.bghub.Views.Main.MainActivity;
@@ -10,25 +13,18 @@ import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.Profile;
 import com.facebook.login.LoginBehavior;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import android.Manifest;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
-
-import dagger.android.AndroidInjection;
 
 public class LoginActivity extends AppCompatActivity implements LoginContract.View {
 
@@ -39,28 +35,47 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
     LoginButton mLoginButton;
     ProgressDialog mProgressDialog = null;
 
+    private static final int RC_SIGN_IN = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-        AndroidInjection.inject(this);
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build());
 
-        mPresenter.start();
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
 
-        //Facebook Login
-        setupFacebookLoginCallBack();
 
-        AndroidInjection.inject(this);
-
-        //TODO move this somewhere appropiate
-        //Check Location Permission and Phone State
-        ActivityCompat.requestPermissions(this,
-                new String[]{
-                        android.Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+//        setContentView(R.layout.activity_login);
+//
+//        AndroidInjection.inject(this);
+//
+//        mPresenter.start();
+//
+//        //Facebook Login
+//        setupFacebookLoginCallBack();
+//
+//        AndroidInjection.inject(this);
+//
+//        //TODO move this somewhere appropiate
+//        //Check Location Permission and Phone State
+//        ActivityCompat.requestPermissions(this,
+//                new String[]{
+//                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
     }
 
     @Override
@@ -70,11 +85,32 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
         super.onDestroy();
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        mCallBackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        mCallBackManager.onActivityResult(requestCode, resultCode, data);
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 
     public void goToMainActivity(){
 
